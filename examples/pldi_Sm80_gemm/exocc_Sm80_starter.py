92cf4a0b718377db5cccb1b9f802093b75d0371d
David Zhao Akeley <dza724@gmail.com>
2026-02-25 22:06:04 -0500

    Update ring buffer pldi_Sm80_gemm

--- examples/pldi_Sm80_gemm/exocc_Sm80_starter.py
+++ examples/pldi_Sm80_gemm/exocc_Sm80_starter.py
@@ -10,7 +10,7 @@ cases = []
 
 
 def add_case(p):
-    p.sync_check(M=256, N=256, K=224)
+    p.sync_check(M=256, N=256, K=320)
     cases.append({
         "algorithm": "gemm",
         "proc": p.name(),
@@ -175,7 +175,7 @@ def starter_simple_smem_gemm(M: size, N: size, K: size, C: f32[M, N] @ CudaGmemL
 add_case(starter_simple_smem_gemm)
 
 
-RING = 4
+RING = 3
 
 
 @proc
@@ -183,7 +183,7 @@ def starter_ring_smem_gemm(M: size, N: size, K: size, C: f32[M, N] @ CudaGmemLin
     assert M % 128 == 0
     assert N % 128 == 0
     assert K % 64 == 0
-    with CudaDeviceFunction(blockDim=128, blocks_per_sm=3):
+    with CudaDeviceFunction(blockDim=128, blocks_per_sm=2):
         for m_task in cuda_tasks(0, (M + 127) / 128):
             for n_task in cuda_tasks(0, (N + 127) / 128):
                 D_rmem: f32[2, 2, 4, 8, 16, 8] @ Sm80_RmemMatrixD_m16n8
@@ -195,57 +195,43 @@ def starter_ring_smem_gemm(M: size, N: size, K: size, C: f32[M, N] @ CudaGmemLin
 
                 A_smem: f16[RING, 2, 128, 8] @ CudaSmemLinear
                 B_smem: f16[RING, 2, 128, 8] @ CudaSmemLinear
-
+                A_rmem: f16[2, 2, 8, 4, 2, 8, 1, 2] @ CudaRmemPacked32
+                B_rmem: f16[2, 2, 8, 4, 2, 8, 1, 2] @ CudaRmemPacked32
                 cg: barrier[128] @ CudaCommitGroup
 
                 for ks in seq(0, K / 16 + RING - 1):
                     A_tile = A[m_task * 128 : m_task * 128 + 128, ks * 16 : ks * 16 + 16]
                     B_tile = B[n_task * 128 : n_task * 128 + 128, ks * 16 : ks * 16 + 16]
 
-                    if ks >= RING - 1:
-                        for mw in cuda_threads(0, 2, unit=2 * cuda_warp):
-                            for nw in cuda_threads(0, 2, unit=cuda_warp):
-                                A_rmem: f16[8, 4, 2, 8, 1, 2] @ CudaRmemPacked32
-                                B_rmem: f16[8, 4, 2, 8, 1, 2] @ CudaRmemPacked32
+                    for mw in cuda_threads(0, 2, unit=2 * cuda_warp):
+                        for nw in cuda_threads(0, 2, unit=cuda_warp):
+                            if ks >= RING - 1:
+                                for s in seq(0, 2, pragma_unroll=0):
+                                    Sm80_ldmatrix_f16(
+                                        A_rmem[mw, nw, :, :, 1, s*4:s*4+4, 0:1, 0:2],
+                                        A_smem[(ks - RING + 1) % RING, 1, mw * 64 + 32 * s : mw * 64 + 32 * s + 32, :],
+                                        nmat0=4, nmat1=1,
+                                    )
+                                    Sm80_ldmatrix_f16(
+                                        B_rmem[mw, nw, :, :, 1, s*4:s*4+4, 0:1, 0:2],
+                                        B_smem[(ks - RING + 1) % RING, 1, nw * 64 + 32 * s : nw * 64 + 32 * s + 32, :],
+                                        nmat0=4, nmat1=1,
+                                    )
+                                for ms in seq(0, 4, pragma_unroll=0):
+                                    for ns in seq(0, 8, pragma_unroll=0):
+                                        Sm80_mma_m16n8(
+                                            D_rmem[mw, nw, ms, ns, :, :],
+                                            A_rmem[mw, nw, :, :, 0, ms * 2 : ms * 2 + 2, 0, :],
+                                            B_rmem[mw, nw, :, :, 0, ns, 0, :],
+                                            D=f32,
+                                            A=f16,
+                                            B=f16,
+                                            K_pack=2,
+                                        )
 
-                                for ki in seq(0, 2):
-                                    Sm80_ldmatrix_f16(
-                                        A_rmem[:, :, ki, 0:4, 0:1, 0:2],
-                                        A_smem[(ks - RING + 1) % RING, ki, mw * 64 + 0 : mw * 64 + 32, :],
-                                        nmat0=4, nmat1=1,
-                                    )
-                                    Sm80_ldmatrix_f16(
-                                        A_rmem[:, :, ki, 4:8, 0:1, 0:2],
-                                        A_smem[(ks - RING + 1) % RING, ki, mw * 64 + 32 : mw * 64 + 64, :],
-                                        nmat0=4, nmat1=1,
-                                    )
-                                    Sm80_ldmatrix_f16(
-                                        B_rmem[:, :, ki, 0:4, 0:1, 0:2],
-                                        B_smem[(ks - RING + 1) % RING, ki, nw * 64 + 0 : nw * 64 + 32, :],
-                                        nmat0=4, nmat1=1,
-                                    )
-                                    Sm80_ldmatrix_f16(
-                                        B_rmem[:, :, ki, 4:8, 0:1, 0:2],
-                                        B_smem[(ks - RING + 1) % RING, ki, nw * 64 + 32 : nw * 64 + 64, :],
-                                        nmat0=4, nmat1=1,
-                                    )
-
-                                for ki in seq(0, 2):
-                                    for ms in seq(0, 4):
-                                        for ns in seq(0, 8):
-                                            Sm80_mma_m16n8(
-                                                D_rmem[mw, nw, ms, ns, :, :],
-                                                A_rmem[:, :, ki, ms * 2 : ms * 2 + 2, 0, :],
-                                                B_rmem[:, :, ki, ns, 0, :],
-                                                D=f32,
-                                                A=f16,
-                                                B=f16,
-                                                K_pack=2,
-                                            )
-
-                    for ki in seq(0, 2):
-                        for tid in cuda_threads(0, 128):
-                            if ks < K / 16:
+                    for tid in cuda_threads(0, 128):
+                        if ks < K / 16:
+                            for ki in seq(0, 2, pragma_unroll=0):
                                 Sm80_cp_async_1d(
                                     A_smem[ks % RING, ki, tid, :], A_tile[tid, ki * 8 : ki * 8 + 8],
                                     size0=8, dst=f16, src=f16
@@ -254,9 +240,36 @@ def starter_ring_smem_gemm(M: size, N: size, K: size, C: f32[M, N] @ CudaGmemLin
                                     B_smem[ks % RING, ki, tid, :], B_tile[tid, ki * 8 : ki * 8 + 8],
                                     size0=8, dst=f16, src=f16
                                 )
-                            Arrive(Sm80_cp_async) >> cg[tid]
-                            Await(cg[tid], cuda_in_order, RING - 1)
+                        Arrive(Sm80_cp_async) >> cg[tid]
+                        Await(cg[tid], cuda_in_order, RING - 2)
                     Fence(cuda_in_order, cuda_in_order)
+
+                    for mw in cuda_threads(0, 2, unit=2 * cuda_warp):
+                        for nw in cuda_threads(0, 2, unit=cuda_warp):
+                            if ks >= RING - 2:
+                                for s in seq(0, 2, pragma_unroll=0):
+                                    Sm80_ldmatrix_f16(
+                                        A_rmem[mw, nw, :, :, 0, s*4:s*4+4, 0:1, 0:2],
+                                        A_smem[(ks - RING + 2) % RING, 0, mw * 64 + 32 * s : mw * 64 + 32 * s + 32, :],
+                                        nmat0=4, nmat1=1,
+                                    )
+                                    Sm80_ldmatrix_f16(
+                                        B_rmem[mw, nw, :, :, 0, s*4:s*4+4, 0:1, 0:2],
+                                        B_smem[(ks - RING + 2) % RING, 0, nw * 64 + 32 * s : nw * 64 + 32 * s + 32, :],
+                                        nmat0=4, nmat1=1,
+                                    )
+                            if ks >= RING - 1:
+                                for ms in seq(0, 4, pragma_unroll=0):
+                                    for ns in seq(0, 8, pragma_unroll=0):
+                                        Sm80_mma_m16n8(
+                                            D_rmem[mw, nw, ms, ns, :, :],
+                                            A_rmem[mw, nw, :, :, 1, ms * 2 : ms * 2 + 2, 0, :],
+                                            B_rmem[mw, nw, :, :, 1, ns, 0, :],
+                                            D=f32,
+                                            A=f16,
+                                            B=f16,
+                                            K_pack=2,
+                                        )
 
                 for tid in cuda_threads(0, 128):
                     Arrive(Sm80_cp_async) >> cg[tid]
