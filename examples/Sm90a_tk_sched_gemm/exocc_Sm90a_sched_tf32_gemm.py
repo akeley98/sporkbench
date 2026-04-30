428a4c33c76ce38213434a48e52446f5ea1c3547
David Zhao Akeley <dza724@gmail.com>
2026-04-29 22:16:29 -0400

    New tf32 Sm90a_tk_sched_gemm with new scheduling

---
+++ examples/Sm90a_tk_sched_gemm/exocc_Sm90a_sched_tf32_gemm.py
@@ -0,0 +1,32 @@
+from exo.platforms.Sm90.tk_gemm_util import *
+
+from dataclasses import replace  # Overrides Exo replace
+
+cases = []
+
+basic_config = GemmConfig(
+    ncta_M=1,
+    ncta_N=1,
+    cta_M=256,
+    cta_N=192,
+    ring_depth=4,
+    swizzle=128,
+    A_type="f32",
+    A_major="row",
+    B_type="f32",
+    B_major="col",
+    C_type="f32",
+    C_major="row",
+    enable_split_k=True,
+    ping_pong=False,
+)
+
+
+m2n2 = schedule_gemm(replace(basic_config, ncta_M=2, ncta_N=2), cases)
+m1n1 = schedule_gemm(replace(basic_config, ncta_M=1, ncta_N=1), cases)
+m1n2 = schedule_gemm(replace(basic_config, ncta_M=1, ncta_N=2), cases)
+m2n1 = schedule_gemm(replace(basic_config, ncta_M=2, ncta_N=1), cases)
+
+
+import json
+json.dump(cases, open(__file__ + ".json", "w"))
